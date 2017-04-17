a6160f65c8ba41661f04fe29b10c4a7c88e9ccf9
Alex Bieg <Alex Bieg>
2017-04-16 19:26:17 -0700

    added basic angular stuff

---
+++ javascript/components/test-component.js
@@ -0,0 +1,9 @@
+angular.module('app').component('testComponent', {
+        template: '<div class="test-component"><h1>Test Component! Hello {{$ctrl.name}}!</h1></div>',
+        bindings: {
+            name: "="
+        },
+        controller: function() {
+            console.log(this);
+        }
+    });
--- javascript/main.js
+++ javascript/main.js
@@ -1,1 +1,12 @@
-console.log("hello world!");
+// External Libaries
+var angular = require('angular');
+
+// Setup angular module
+angular.module('app', [])
+    .controller("MainCtrl", function($scope) {
+        $scope.name = "Alex";
+    });
+
+
+// Angular Components
+require('./components/test-component.js');
